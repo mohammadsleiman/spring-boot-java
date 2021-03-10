4171839988da4c64bd2a5bfcc1c3e657b251bb63
modysleiman <modysleiman@gmail.com>
2021-03-09 20:46:37 -0800

    first push

---
+++ src/main/java/com/mohammadsleiman/app/HelloWorldSpringBootApp.java
@@ -0,0 +1,13 @@
+package com.mohammadsleiman.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class HelloWorldSpringBootApp {
+	public static void main(String[]args) {
+		SpringApplication.run(HelloWorldSpringBootApp.class, args);
+	}
+
+}
---
+++ src/main/java/com/mohammadsleiman/app/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.mohammadsleiman.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloWorldController {
+	
+	@RequestMapping(value="/")
+	public String hello() {
+		return "Hello World! :)";
+	}
+}
